5e5a9cd675f4e70a6d9374eed8922206d79f46dd
im-minion <vaibhavminiyar@gmail.com>
2017-06-24 15:10:09 +0530

    adWorking

--- app/src/main/java/com/example/vaibhav/simpleblogapp/ShowActivity/MainActivity.java
+++ app/src/main/java/com/example/vaibhav/simpleblogapp/ShowActivity/MainActivity.java
@@ -2,16 +2,12 @@ package com.example.vaibhav.simpleblogapp.ShowActivity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,9 +29,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mBlogList;
@@ -85,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                 BloViewHolder.class,
                 mDatabase) {
             @Override
-            protected void populateViewHolder(BloViewHolder viewHolder, Blog model, int position) {
+            protected void populateViewHolder(BloViewHolder viewHolder, final Blog model, final int position) {
                 viewHolder.setTitle(model.getTitle());
                 viewHolder.setDesc(model.getDESCRIPTION());
                 viewHolder.setUsername(model.getUsername());
