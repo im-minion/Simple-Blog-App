0316dde8f79bd05dcd7d96b9eb262aeee9d5157f
im-minion <vaibhavminiyar@gmail.com>
2017-07-07 22:56:26 +0530

    smll fixex

--- app/src/main/java/com/example/vaibhav/simpleblogapp/ProfileExists.java
+++ app/src/main/java/com/example/vaibhav/simpleblogapp/ProfileExists.java
@@ -1,10 +1,12 @@
 package com.example.vaibhav.simpleblogapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.vaibhav.simpleblogapp.ShowActivity.MainActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
 
@@ -20,10 +22,16 @@ public class ProfileExists extends AppCompatActivity {
         iv = (ImageView) findViewById(R.id.imageView);
         String uname = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
         tv.setText("Logged in as " + uname);
-        if (FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl() == null){
+        if (FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl() == null) {
             Picasso.with(ProfileExists.this).load("http://www.gpp-kavkaz.ru/images/no_avatar.jpg?crc=238954602").into(iv);
-        }else {
+        } else {
             Picasso.with(ProfileExists.this).load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl()).into(iv);
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(ProfileExists.this, MainActivity.class));
     }
 }
